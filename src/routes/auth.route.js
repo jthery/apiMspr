e9042ee012d74a9a60d0f8d8dda4b897c0c2a002
jthery <j.thery2311@outlook.fr>
2020-03-15 20:15:31 +0100

    swagger for requests register/login/coupons

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,10 +5,64 @@ var router = express.Router();
 // Controller declaration
 const AuthController = require('../controllers/auth.controller.js');
 
-//LOGIN : Crud method, route and controller function 
+/**
+ * @swagger
+ *
+ * /api/login:
+ *   post:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: Email to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: pwd
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login has been established successfully
+ */
 router.post('/login', AuthController.login);
 
-//REGISTER : Crud method, route and controller function 
+/**
+ * @swagger
+ *
+ * /api/register:
+ *   post:
+ *     description: Register to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: nom
+ *         description: user's name.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: prenom
+ *         description: user's firstname.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: Email to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: pwd
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: register has been created successfully
+ */
 router.post('/register', AuthController.register);
 
 // Export routes
