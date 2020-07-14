fea2b99fb1762aa832dcbb80b35dba201de635da
jthery <j.thery2311@outlook.fr>
2020-07-14 23:52:57 +0200

    add test coupon

--- test/coupon.test.js
+++ test/coupon.test.js
@@ -22,4 +22,19 @@ describe('Tests concernant les coupons', function () {
         });
     });
 
+    describe('return get coupon by code_coupon', function () {
+        it('Vérifie qu\'il y a chaque propriété pour le get /couponByCodeCoupon', function () {
+            api.get(`${CONFIG.uri_prefix_main}/couponByCodeCoupon`)
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.status).to.equal(200);
+                    expect(res.body[0]).to.have.property('couponId');
+                    expect(res.body[0]).to.have.property('discount');
+                    expect(res.body[0]).to.have.property('description');
+                    expect(res.body[0]).to.have.property('date_debut');
+                    expect(res.body[0]).to.have.property('date_fin');
+                });
+        });
+    });
+
 });
